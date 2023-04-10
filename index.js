767c6a01cf717ecfd51efe0d5a5c263a580d5688
Kayla Copeland <kaylabebusy@gmail.com>
2023-04-10 15:12:04 -0400

    lab done

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    callback()
+}
+
+function brookesFunction() {
+    console.log("Brooke")
+}
+
+function returnsANamedFunction() {
+    return brookesFunction
+}
+
+function returnsAnAnonymousFunction() {
+return function() {
+    
+}
+}
